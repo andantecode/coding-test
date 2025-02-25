e54b5e1799b581524cebe43cf73b41395d4b0abd
andantecode <kb721a@naver.com>
2025-02-25 17:23:03 +0900

    modify: 1,2,3 더하기 15989

--- 동적프로그래밍/1,2,3_더하기_4.15989.py
+++ 동적프로그래밍/1,2,3_더하기_4.15989.py
@@ -39,14 +39,10 @@ dp = [1] * (max(targets) + 1)
 for i in range(2, len(dp)):
     dp[i] += dp[i - 2]
 
-print(dp)
-
 # 3을 추가해서 만드는 경우의 수는 dp[i-3]의 경우의 수에 +3을 해서 만들 수 있음
 # 5: (1+1)+3, (2)+3
 for i in range(3, len(dp)):
     dp[i] += dp[i - 3]
-
-print(dp)
 
 for target in targets:
     print(dp[target])
